f21cecfb03e3a21f4e0882011df188ac5e2f10c6
zinave <36947601@qq.com>
2017-09-21 13:27:36 +0800

    淘宝工具集合

---
+++ src/main/java/com/qimencloud/api/response/HshcRiskcontolCustomerReturnResponse.java
@@ -0,0 +1,45 @@
+package com.qimencloud.api.response;
+
+import com.qimencloud.api.AbstractQimenCloudResponse;
+import com.taobao.api.internal.mapping.ApiField;
+
+/**
+ * TOP API(QimenCloud): hshc.riskcontol.customer.return response.
+ * 
+ * @author top auto create
+ * @since 1.0, null
+ */
+public class HshcRiskcontolCustomerReturnResponse extends AbstractQimenCloudResponse {
+
+	private static final long serialVersionUID = 3328288856596438159L;
+
+	/** 
+	 * 信息
+	 */
+	@ApiField("message")
+	private String message;
+
+	/** 
+	 * 成功
+	 */
+	@ApiField("success")
+	private Boolean success;
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getMessage( ) {
+		return this.message;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+	public Boolean getSuccess( ) {
+		return this.success;
+	}
+	
+
+
+}
---
+++ src/main/java/com/qimencloud/api/response/HshcRiskcontolOrdersReturnResponse.java
@@ -0,0 +1,45 @@
+package com.qimencloud.api.response;
+
+import com.qimencloud.api.AbstractQimenCloudResponse;
+import com.taobao.api.internal.mapping.ApiField;
+
+/**
+ * TOP API(QimenCloud): hshc.riskcontol.orders.return response.
+ * 
+ * @author top auto create
+ * @since 1.0, null
+ */
+public class HshcRiskcontolOrdersReturnResponse extends AbstractQimenCloudResponse {
+
+	private static final long serialVersionUID = 1192136346679898354L;
+
+	/** 
+	 * 一级响应参数
+	 */
+	@ApiField("message")
+	private String message;
+
+	/** 
+	 * 一级响应消息
+	 */
+	@ApiField("success")
+	private Boolean success;
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getMessage( ) {
+		return this.message;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+	public Boolean getSuccess( ) {
+		return this.success;
+	}
+	
+
+
+}
